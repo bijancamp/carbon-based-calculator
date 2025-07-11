106871b16c5541cc2fdc0ce029a6e65075113a22
Bijan "Biji" Camp <8800015+bijancamp@users.noreply.github.com>
2025-07-11 03:56:20 +0000

    Ensure footer is fixed for iOS Safari

--- src/web/src/components/AppLayout.tsx
+++ src/web/src/components/AppLayout.tsx
@@ -27,7 +27,7 @@ function AppLayout({ children }: { children: ReactNode }) {
   };
 
   return (
-    <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+    <Box sx={{ minHeight: { xs: '100dvh', sm: '100vh' }, display: 'flex', flexDirection: 'column' }}>
       <AppBar position="fixed">
         <Toolbar>
           {isMobile && (
